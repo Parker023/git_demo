0558b3d288642dc8c93056f87c3ef84f79f0137e
Shanmukha Anirudh <tallurishanmukha.anirudh@clarivate.com>
2022-08-20 16:33:26 +0530

    add file changed

--- add.js
+++ add.js
@@ -1,6 +1,6 @@
 function add() {
-var num1=document.getElementById("fn").value;
-var num2=document.getElementById("sn").value;
+var num1=document.getElementById("fn").value
+var num2=document.getElementById("sn").value
 var result = parseInt(num1)+parseInt(num2)
 document.getElementById("result").value=result
 }
