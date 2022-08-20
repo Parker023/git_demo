0c2a0df62b00da6ba44337bd53176de4059baa2e
Talluri Shanmukha Anirudh <tallurishanmukha.anirudh@clarivate.com>
2022-08-20 15:37:41 +0530

    addition file created

---
+++ add.js
@@ -0,0 +1,7 @@
+function add() {
+var num1=document.getElementById("fn").value;
+var num2=document.getElementById("sn").value;
+var result = parseInt(num1)+parseInt(num2)
+document.getElementById("result").value=result
+}
+
