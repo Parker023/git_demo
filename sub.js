14fa6701ad5908e5d617c69ed9029d7f1b431c3b
Talluri Shanmukha Anirudh <tallurishanmukha.anirudh@clarivate.com>
2022-08-20 15:42:46 +0530

    subtraction file created

---
+++ sub.js
@@ -0,0 +1,6 @@
+function sub() {
+var num1=document.getElementById("fn").value;
+var num2=document.getElementById("sn").value;
+var result = parseInt(num1)-parseInt(num2)
+document.getElementById("result").value=result
+}
